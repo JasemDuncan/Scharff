6efc59373ca4a6f8a1eb6517a20a5945afdf3799
Jasem Duncan <jasemvalencia@gmail.com>
2024-05-01 18:52:13 -0500

    Create mongodb connection

---
+++ api-rest-dark-store/index.js
@@ -0,0 +1,16 @@
+//dependency import
+const connection = require("./database/connection");
+//welcome message
+console.log("API NODE for Dark Store loaded")
+//db conection
+connection();
+
+//create node server
+
+//configure cors
+
+//convert data body to js objects
+
+//load conf routes
+
+//server listening for http request
